4b41aaca7a80b4c4aad3a0e2ccc1f840940115a3
Jack Peng <pengu@Jacks-MacBook-Air.local>
2022-05-31 16:32:38 +1000

    changes

--- week1/random.c
+++ week1/random.c
@@ -6,26 +6,3 @@ int main() {
    printf("Hello, World!");
    return 0;
 }
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
